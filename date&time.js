172b18f90fd8f50411f19398a9718c98bffb32ec
DvDyziek <146833266+DvDyziek@users.noreply.github.com>
2023-11-16 15:50:11 +0100

    1

---
+++ date&time.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<script>
+const today - new date();
+const year - today.getfullyear();
+const day - today.getday();
+const month - today.getmonth();
+
+console.log( day, ' ', month,' ', year );
+
+</script>
+
+</head>
+<body>
+    
+
+</body>
+</html>
